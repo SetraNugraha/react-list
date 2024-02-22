3629b9b674750cfe17f0ecbf24453a944b882df9
Setra Nugraha Putra Suma <setranugraha75@gmail.com>
2024-02-22 21:26:50 +0700

    add total belanjaan & harga & format Rp

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@
 import { useState, useEffect } from 'react'
 import { BsCheckSquareFill } from 'react-icons/bs'
 import { BsTrash3Fill } from 'react-icons/bs'
+import numeral from 'numeral'
 
 export default function App() {
   const [belanjaan, setBelanjaan] = useState([])
@@ -47,17 +48,17 @@ export default function App() {
     return (
       <>
         <tr className=" bg-gray-800">
-          <th scope="row" className="px-6 py-4 font-medium whitespace-nowrap text-white">
+          <th scope="row" className="px-4 py-4 font-medium whitespace-nowrap text-white">
             {nama}
           </th>
           <td className="px-6 py-4">{jumlah}</td>
-          <td className="px-6 py-4">Rp. {harga}</td>
+          <td className="px-6 py-4">{harga}</td>
           <td className="px-6 py-4">
-            <button className='p-2 bg-green-600'>
+            <button className="p-2 bg-green-600">
               <BsCheckSquareFill />
             </button>
-            <button className='p-2 bg-red-500'>
-              <BsTrash3Fill/>
+            <button className="p-2 bg-red-500">
+              <BsTrash3Fill />
             </button>
           </td>
         </tr>
@@ -66,6 +67,15 @@ export default function App() {
   }
 
   const data = getDataBelanjaan()
+
+  const getTotalHarga = () => {
+    let total = 0
+    data.forEach((item) => {
+      total += parseInt(item.harga)
+    })
+
+    return total
+  }
 
   return (
     <>
@@ -84,7 +94,7 @@ export default function App() {
             <label htmlFor="nama" className="font-semibold text-lg">
               Nama :
             </label>
-            <input type="text" name="nama" id="nama" className="h-10 border border-slate-600 rounded-lg my-2 px-5 text-lg" value={formData.nama} onChange={handleChange} />
+            <input type="text" name="nama" id="nama" className="h-10 border border-slate-600 rounded-lg my-2 px-5 text-lg" value={formData.nama} onChange={handleChange} required />
           </div>
 
           {/* Jumlah */}
@@ -92,7 +102,7 @@ export default function App() {
             <label htmlFor="jumlah" className="font-semibold text-lg">
               Jumlah :
             </label>
-            <input type="text" name="jumlah" id="jumlah" className="h-10 border border-slate-600 rounded-lg my-2 px-5 text-lg" value={formData.jumlah} onChange={handleChange} />
+            <input type="text" name="jumlah" id="jumlah" className="h-10 border border-slate-600 rounded-lg my-2 px-5 text-lg" value={formData.jumlah} onChange={handleChange} required />
           </div>
 
           {/* Perkiraan Harga */}
@@ -100,7 +110,7 @@ export default function App() {
             <label htmlFor="harga" className="font-semibold text-lg">
               Perkiraan Harga :
             </label>
-            <input type="number" name="harga" id="harga" className="h-10 border border-slate-600 rounded-lg my-2 px-5 text-lg" value={formData.harga} onChange={handleChange} />
+            <input type="number" name="harga" id="harga" className="h-10 border border-slate-600 rounded-lg my-2 px-5 text-lg" value={formData.harga} onChange={handleChange} required />
           </div>
 
           <div className="text-center">
@@ -115,7 +125,7 @@ export default function App() {
       {/* START LIST */}
       <div className="px-2">
         <div className="relative overflow-x-auto">
-          <table className="w-full text-sm text-left rtl:text-right text-gray-300 ">
+          <table className="w-full my-10 text-sm text-left rtl:text-right text-gray-300 ">
             <thead className="text-xs uppercase bg-gray-700 text-gray-300">
               <tr>
                 <th scope="col" className="px-6 py-3 rounded-s-lg">
@@ -125,7 +135,7 @@ export default function App() {
                   Jumlah
                 </th>
                 <th scope="col" className="px-6 py-3">
-                  Perkiraan Harga
+                  Perkiraan Harga (Rp)
                 </th>
                 <th scope="col" className="px-6 py-3 rounded-e-lg">
                   Aksi
@@ -133,17 +143,27 @@ export default function App() {
               </tr>
             </thead>
             <tbody>
-              {data.map((item, index) => {
-                return <ListBelanjaan key={index} nama={item.nama} jumlah={item.jumlah} harga={item.harga} />
-              })}
+              {data.length > 0 ? (
+                data.map((item, index) => {
+                  return <ListBelanjaan key={index} nama={item.nama} jumlah={item.jumlah} harga={numeral(item.harga).format(0,0)} />
+                })
+              ) : (
+                <tr>
+                  <td colSpan={4} className="text-center text-gray-300 font-semibold bg-gray-800 py-4">
+                    Belum Ada List Belanjaan
+                  </td>
+                </tr>
+              )}
             </tbody>
             <tfoot>
               <tr className="font-semibold text-gray-300 bg-gray-700">
                 <th scope="row" className="px-6 py-3 text-base rounded-s-lg">
                   TOTAL :
                 </th>
-                <td className="px-6 py-3">3</td>
-                <td className="px-6 py-3 text-center rounded-e-lg" colSpan={2}>Rp. 21,000</td>
+                <td className="px-6 py-3 text-center">{data.length} Belanjaan</td>
+                <td className="px-6 py-3 text-center rounded-e-lg" colSpan={2}>
+                  Rp. {numeral(getTotalHarga()).format('0,0')}
+                </td>
               </tr>
             </tfoot>
           </table>
